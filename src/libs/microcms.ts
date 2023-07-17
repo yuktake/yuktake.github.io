ca8168645d43d1e0194498f97fef0bfc4fcd06a9
yukitake <koshi.yukitake@gmail.com>
2023-07-18 02:04:42 +0900

    キャッシュによってCMSのデータが反映されないのを修正

--- src/libs/microcms.ts
+++ src/libs/microcms.ts
@@ -8,6 +8,15 @@ import type {
 export const client = createClient({
   serviceDomain: process.env.SERVICE_DOMAIN || "",
   apiKey: process.env.API_KEY || "",
+  customFetch: (input, init) => {
+    if (typeof input === 'string') {
+      const newInput = new URL(input)
+      const time = new Date()
+      newInput.searchParams.set('cacheclearparam', `${time.getMinutes()}`)
+      return fetch(newInput.href, init)
+    }
+    return fetch(input, init)
+  },
 });
 
 //ブログの型定義
